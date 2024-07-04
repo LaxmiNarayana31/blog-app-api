4460e9fe58dc613311590e722a0232407b903c22
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-07-04 23:38:32 +0530

    Blog operation routes

--- routers/blog.py
+++ routers/blog.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
-import schemas
-import oauth2
-from models import Blog
+import schemas, oauth2
 from database import get_db
 from repository import blogfile
+
+
+
+from fastapi_pagination import Page, paginate
+
 
 
 router = APIRouter(
@@ -15,10 +18,10 @@ router = APIRouter(
 
 
 
+# GET -> get all data from database
 @router.get('/blog', response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blogfile.get_all(db)
-
 
 
 @router.get('/blog/{id}', status_code = 200, response_model=schemas.ShowBlog)
@@ -26,11 +29,9 @@ def show(id:int, db: Session = Depends(get_db)):
     return blogfile.show(id, db)
 
 
-
 @router.post('/blog')
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return blogfile.create(request, db)
-
 
 
 @router.put('/blog/{id}')    #  tags=["blogs"] -> These sre called Doc tag 
@@ -42,3 +43,21 @@ def update(id:int, request: schemas.BlogUpdate, db: Session = Depends(get_db)):
 @router.delete('/blog/{id}')
 def delete(id, db: Session = Depends(get_db)):
     return blogfile.delete(id,db)
+
+
+
+
+# fetch blogs by user id 
+# implement pagination
+
+# @router.get('/blogs/{user_id}', response_model=List[schemas.ShowBlog])
+# def get_user_blogs(user_id: int, skip: int = 0, limit: int = 5, db: Session = Depends(get_db)):
+#     return blogfile.get_user_blogs(user_id, skip, limit, db)
+
+
+
+
+@router.get('/blogs/{user_id}', response_model=Page[schemas.ShowBlog])
+def get_user_blogs(user_id: int, db: Session = Depends(get_db)):
+    blogs = blogfile.get_user_blogs(user_id, db)
+    return paginate(blogs)
