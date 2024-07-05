56bca7387dabc5c642c58d533f3412a878325ae3
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2024-07-05 22:57:36 +0530

    User files

--- repository/userfile.py
+++ repository/userfile.py
@@ -1,13 +1,13 @@
 from sqlalchemy.orm import Session
 import schemas
 from fastapi import HTTPException, status
-from models import User
+from models.user_model import UserModel
 from hashing import Hash
 
 
 
 def create_new_user(request: schemas.User, db:Session):
-    new_user = User(
+    new_user = UserModel(
         name = request.name,
         email = request.email,
         password = Hash.bcrypt(request.password)
@@ -19,9 +19,10 @@ def create_new_user(request: schemas.User, db:Session):
 
 
 
+
 def show_user(id:int, db:Session):
-    user = db.query(User).filter(User.id == id).first()
+    user = db.query(UserModel).filter(UserModel.id == id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} is not available")
-    
+
     return user
